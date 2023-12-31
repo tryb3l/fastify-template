8048300df30c94c197e97f199c12feb32c1add2b
tryb3l <56679619+tryb3l@users.noreply.github.com>
2023-12-31 22:51:54 +0100

    Add fastify-env plugin for loading configuration from environment variables

--- plugins/config.js
+++ plugins/config.js
@@ -1,6 +1,19 @@
+"use strict";
+/**
+ * Fastify plugin for loading configuration from environment variables.
+ * @module plugins/config
+ */
+
 const fp = require("fastify-plugin");
 const fastifyEnv = require("@fastify/env");
+
 module.exports = fp(
+  /**
+   * Registers the fastify-env plugin to load configuration from environment variables.
+   * @param {Object} fastify - The Fastify instance.
+   * @param {Object} opts - The plugin options.
+   * @param {Function} next - The callback function to continue the plugin registration.
+   */
   function (fastify, opts, next) {
     fastify.register(fastifyEnv, {
       confKey: "configSecret",
