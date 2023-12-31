5965f3eb732973eef8114ab517254686d0b441a0
tryb3l <56679619+tryb3l@users.noreply.github.com>
2023-12-31 22:40:22 +0100

    Added error handler plugin

---
+++ plugins/error-handler.js
@@ -0,0 +1,14 @@
+const fp = require("fastify-plugin");
+module.exports = fp(function (fastify, opts, next) {
+  fastify.setErrorHandler(function (error, request, reply) {
+    if (reply.statusCode >= 500) {
+      req.log.error({ req, res: reply, err: err }, err?.message);
+      reply.send(`Fatal error. Contact the support team. Id
+            ${req.id}`);
+      return;
+    }
+    req.log.info({ req, res: reply, err: err }, err?.message);
+    reply.send(err);
+  });
+  next();
+});
