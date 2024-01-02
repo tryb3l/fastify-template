ea917ac6b8f30762fd1eed42a836bdd7aa15d6d2
tryb3l <56679619+tryb3l@users.noreply.github.com>
2024-01-02 19:52:07 +0100

    Refactor configuration loading

--- configs/config.js
+++ configs/config.js
@@ -1,10 +1,21 @@
+"use strict";
 /**
- * Loads the configuration for the Fastify application.
- *
- * @param {FastifyInstance} fastify - The Fastify instance.
- * @param {Object} opts - The options object.
- * @returns {Promise<void>} - A promise that resolves when the configuration is loaded.
+ * Fastify plugin for loading environment variables.
+ * @module configLoader
  */
+
+const fp = require("fastify-plugin");
+/**
+ * Retrieves environment variables and sets them on the Fastify instance.
+ * @param {import('fastify').FastifyInstance} fastify - The Fastify instance.
+ * @param {object} options - The options object.
+ * @param {string} options.envFile - The path to the environment file.
+ * @param {boolean} [options.overrideExisting=false] - Whether to override existing environment variables.
+ */
+/**
+ * @typedef {import("@fastify/env")} fastifyEnv
+ */
+const fastifyEnv = require("@fastify/env");
 module.exports = fp(async function configLoader(fastify, opts) {
   await fastify.register(fastifyEnv, {
     confKey: "secrets",
