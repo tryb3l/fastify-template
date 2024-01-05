47d60159b526a05ce759f3e1aa068e9f2edae01b
tryb3l <56679619+tryb3l@users.noreply.github.com>
2024-01-05 23:53:39 +0100

    Refactor configLoader function in config.js

--- configs/config.js
+++ configs/config.js
@@ -16,16 +16,21 @@ const fp = require("fastify-plugin");
  * @typedef {import("@fastify/env")} fastifyEnv
  */
 const fastifyEnv = require("@fastify/env");
-module.exports = fp(async function configLoader(fastify, opts) {
-  await fastify.register(fastifyEnv, {
-    confKey: "secrets",
-    data: opts.configData,
-    schema: fastify.getSchema("schema:dotenv"),
-  });
-  fastify.decorate("config", {
-    mongo: {
-      forceClose: true,
-      url: fastify.secrets.MONGO_URL,
-    },
-  });
-});
+module.exports = fp(
+  async function configLoader(fastify, opts) {
+    await fastify.register(fastifyEnv, {
+      confKey: "secrets",
+      data: opts.configData,
+      schema: fastify.getSchema("schema:dotenv"),
+    });
+    fastify.decorate("config", {
+      mongo: {
+        forceClose: true,
+        url: fastify.secrets.MONGO_URL,
+      },
+    });
+  },
+  {
+    dependencies: ["application-config"],
+  },
+);
