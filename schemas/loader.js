1c5bb1f152438e31fab4817bba4bb963d1bd3f93
tryb3l <56679619+tryb3l@users.noreply.github.com>
2024-01-05 23:52:49 +0100

    Add new JSON schemas for status parameters and skip

--- schemas/loader.js
+++ schemas/loader.js
@@ -3,11 +3,11 @@
  * Fastify plugin function. It must add all the JSON schemas we will need for our application
  * @typedef {import('fastify').FastifyPluginCallback} FastifyPluginCallback
  */
+
 const fp = require("fastify-plugin");
-module.exports = fp(
-  function (fastify, opts, next) {
-    fastify.addSchema(require("./dotenv.json"));
-    next();
-  },
-  { name: "application-schemas" },
-);
+
+module.exports = fp(function schemaLoaderPlugin(fastify, opts, next) {
+  fastify.addSchema(require("./dotenv.json"));
+
+  next();
+});
