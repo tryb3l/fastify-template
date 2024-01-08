03c45bfafe40573525c9bb4ab12bbb87f1214706
tryb3l <56679619+tryb3l@users.noreply.github.com>
2024-01-08 16:15:00 +0100

    Remove unused plugins and update Swagger configuration

--- plugins/swagger.js
+++ plugins/swagger.js
@@ -1,18 +1,9 @@
 "use strict";
+const fp = require("fastify-plugin");
 
-const fp = require('fastify-plugin')
-const fastifySwagger = require('@fastify/swagger')
-const pkg = require('../package.json')
-/**
- * Registers the Swagger plugin for Fastify.
- *
- * @param {object} fastify - The Fastify instance.
- * @param {object} opts - The options object.
- * @returns {Promise<void>} - A promise that resolves when the plugin is registered.
- */
 module.exports = fp(
   async function swaggerPlugin(fastify, opts) {
-    fastify.register(require("@fastify/swagger"), {
+    await fastify.register(require("@fastify/swagger"), {
       swagger: {
         info: {
           title: "Fastify app",
@@ -21,7 +12,7 @@ module.exports = fp(
         },
       },
     });
-    fastify.register(require("@fastify/swagger-ui"), {
+    await fastify.register(require("@fastify/swagger-ui"), {
       routePrefix: "/docs",
       exposeRoute: fastify.secrets.NODE_ENV !== "production",
     });
