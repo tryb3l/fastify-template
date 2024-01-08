032a3570668ad731d0517a743322e79aaeeeda31
tryb3l <56679619+tryb3l@users.noreply.github.com>
2024-01-08 16:16:30 +0100

    Delete unnecessary schema files

--- routes/example/schemas/loader.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-
-const fp = require("fastify-plugin");
-module.exports = fp(async function schemaLoaderPlugin(fastify, opts) {
-  fastify.addSchema(require("./list-query.json"));
-  fastify.addSchema(require("./create-body.json"));
-  fastify.addSchema(require("./create-response.json"));
-  fastify.addSchema(require("./status-params.json"));
-});
