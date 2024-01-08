b7152d557b1df6f8525f7680c5bdfaa7c201cc2c
tryb3l <56679619+tryb3l@users.noreply.github.com>
2024-01-08 16:18:13 +0100

    Refactor schemaLoaderPlugin function

--- schemas/loader.js
+++ schemas/loader.js
@@ -1,13 +1,5 @@
-"use strict";
-/**
- * Fastify plugin function. It must add all the JSON schemas we will need for our application
- * @typedef {import('fastify').FastifyPluginCallback} FastifyPluginCallback
- */
-
 const fp = require("fastify-plugin");
-
-module.exports = fp(function schemaLoaderPlugin(fastify, opts, next) {
+module.exports = fp(function (fastify, opts, next) {
   fastify.addSchema(require("./dotenv.json"));
-
   next();
 });
