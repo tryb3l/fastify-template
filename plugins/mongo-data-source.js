ee47e3e8d7b40d5fc0511197223e9201b7c5a087
tryb3l <56679619+tryb3l@users.noreply.github.com>
2024-01-08 16:13:56 +0100

    Refactor mongo-data-source plugin registration

--- plugins/mongo-data-source.js
+++ plugins/mongo-data-source.js
@@ -1,14 +1,11 @@
 "use strict";
-/**
- * Fastify plugin for connecting to a MongoDB data source.
- * @param {import('fastify').FastifyInstance} fastify - The Fastify instance.
- * @param {Object} opts - The plugin options.
- * @param {Object} opts.mongo - The MongoDB connection options.
- */
 
 const fp = require("fastify-plugin");
 const fastifyMongo = require("@fastify/mongodb");
 
-module.exports = fp(async function (fastify, opts) {
-  fastify.register(fastifyMongo, opts.mongo);
-});
+module.exports = fp(
+  async function datasourcePlugin(fastify, opts) {
+    fastify.register(fastifyMongo, opts.mongo);
+  },
+  { dependencies: ["application-config"] },
+);
