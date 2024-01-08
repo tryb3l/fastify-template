0b7f91f51bbda9452a6eba94bd0290f83509efa7
tryb3l <56679619+tryb3l@users.noreply.github.com>
2024-01-08 16:13:42 +0100

    Refactor error handler plugin

--- plugins/error-handler.js
+++ plugins/error-handler.js
@@ -1,16 +1,15 @@
-'use strict'
+"use strict";
 
-const fp = require('fastify-plugin')
+const fp = require("fastify-plugin");
 module.exports = fp(function (fastify, opts, next) {
-  fastify.setErrorHandler(function (error, request, reply) {
+  fastify.setErrorHandler((err, req, reply) => {
     if (reply.statusCode >= 500) {
-      req.log.error({ req, res: reply, err }, err?.message)
-      reply.send(`Fatal error. Contact the support team. Id
-            ${req.id}`)
-      return
+      req.log.error({ req, res: reply, err: err }, err?.message);
+      reply.send(`Fatal error. Contact the support team. Id${req.id}`);
+      return;
     }
-    req.log.info({ req, res: reply, err }, err?.message)
-    reply.send(err)
-  })
-  next()
-})
+    req.log.info({ req, res: reply, err: err }, err?.message);
+    reply.send(err);
+  });
+  next();
+});
