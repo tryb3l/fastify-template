8d9cd78f2888c301f702a487e1a646cd98e2cf8e
tryb3l <56679619+tryb3l@users.noreply.github.com>
2024-01-13 23:59:14 +0100

    Refactor authentication routes and add new endpoints

--- routes/auth/routes.js
+++ routes/auth/routes.js
@@ -28,8 +28,8 @@ module.exports = fp(async function applicationAuth(fastify, opts) {
           salt,
           hash,
         });
-        request.log.info({ userId: newUserId }, "User registered");
 
+        request.log.info({ userId: newUserId }, "User registered");
         reply.code(201);
         return { registered: true };
       } catch (error) {
@@ -37,6 +37,53 @@ module.exports = fp(async function applicationAuth(fastify, opts) {
         reply.code(500);
         return { registered: false };
       }
+    },
+  });
+  fastify.post("/authenticate", {
+    schema: {
+      // [2.1]
+      body: fastify.getSchema("schema:auth:register"),
+      response: {
+        200: fastify.getSchema("schema:auth:token"),
+      },
+    },
+    handler: async function authenticateHandler(request, reply) {
+      const user = await this.usersDataSource.readUser(request.body.username);
+      if (!user) {
+        // if we return 404, it'll make possible case to find out which users are registered
+        const err = new Error("Wrong credentials provided");
+        err.statusCode = 401;
+        throw err;
+      }
+      const { hash } = await generateHash(request.body.password, user.salt);
+      if (hash !== user.hash) {
+        const err = new Error("Wrong credentials provided");
+        err.statusCode = 401;
+        throw err;
+      }
+      request.user = user;
+      return refreshHandler(request, reply);
+    },
+  });
+  fastify.post("/refresh", {
+    onRequest: fastify.authenticate,
+    schema: {
+      headers: fastify.getSchema("schema:auth:token-header"),
+      response: {
+        200: fastify.getSchema("schema:auth:token"),
+      },
+    },
+    handler: refreshHandler,
+  });
+  async function refreshHandler(request, reply) {
+    const token = await request.generateToken();
+    return { token };
+  }
+  fastify.post("/logout", {
+    onRequest: fastify.authenticate,
+    handler: async function logoutHandler(request, reply) {
+      request.revokeToken();
+      reply.code(204);
     },
   });
 });
