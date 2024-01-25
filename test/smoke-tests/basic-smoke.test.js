da82f8ee6b580e18f27cdb29df5db59ee66339b2
tryb3l <56679619+tryb3l@users.noreply.github.com>
2024-01-25 23:59:24 +0100

    Refactor smoke test and add environment parameter

--- test/smoke-tests/basic-smoke.test.js
+++ test/smoke-tests/basic-smoke.test.js
@@ -1,63 +1,20 @@
 "use strict";
 
+const fcli = require("fastify-cli/helper");
+const startArgs = "-l info --options app.js";
 const t = require("tap");
-const { buildApp } = require("../helper");
-const dockerHelper = require("./helper-docker");
-
-const docker = dockerHelper();
-const { Containers } = dockerHelper;
-
-t.before(async function before() {
-  await docker.startContainer(mongo);
-});
-
-t.teardown(async () => {
-  await docker.stopContainer(dockerHelper.Containers.mongo);
-});
-
-t.test("the alive route is online", async (t) => {
-  const app = await buildApp(t, {
-    MONGO_URL: "mongodb://localhost:27017/basis-test-db",
-  });
-  const response = await app.inject({
-    method: "GET",
-    url: "/",
-  });
-  t.same(response.json(), { root: true });
-});
 
 t.test("the application should start", async (t) => {
-  const app = await buildApp(t, {
-    MONGO_URL: "mongodb://localhost:27017/basis-test-db",
+  const envParam = {
+    NODE_ENV: "test",
+    MONGO_URL: "mongodb://localhost:27017/test",
+  };
+  const app = await fcli.build(startArgs, {
+    configData: envParam,
+  });
+  t.teardown(() => {
+    app.close();
   });
   await app.ready();
   t.pass("the application is ready");
-});
-
-t.test("the application should not start", async (mainTest) => {
-  mainTest.test("if there are missing ENV vars", async (t) => {
-    try {
-      await buildApp(t, {
-        NODE_ENV: "test",
-        MONGO_URL: undefined,
-      });
-      t.fail("the server must not start");
-    } catch (error) {
-      t.ok(error, "error must be set");
-      t.match(error.message, "required property 'MONGO_URL'");
-    }
-  });
-
-  mainTest.test("when mongodb is unreachable", async (t) => {
-    try {
-      await buildApp(t, {
-        NODE_ENV: "test",
-        MONGO_URL: "mongodb://localhost:27099/test",
-      });
-      t.fail("the server must not start");
-    } catch (error) {
-      t.ok(error, "error must be set");
-      t.match(error.message, "connect ECONNREFUSED");
-    }
-  });
 });
