47f09f1d63dd892e83745805febf98f8a0ec591e
tryb3l <56679619+tryb3l@users.noreply.github.com>
2024-01-31 21:03:56 +0100

    Refactor smoke test setup

--- test/smoke-tests/basic-smoke.test.js
+++ test/smoke-tests/basic-smoke.test.js
@@ -9,12 +9,16 @@ t.test("the application should start", async (t) => {
     NODE_ENV: "test",
     MONGO_URL: "mongodb://localhost:27017/test",
   };
+
   const app = await fcli.build(startArgs, {
     configData: envParam,
   });
+
   t.teardown(() => {
     app.close();
   });
+
   await app.ready();
+
   t.pass("the application is ready");
 });
