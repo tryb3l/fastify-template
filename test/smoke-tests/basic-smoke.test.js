0215eca7767270d2d082f18fa39af9feb204f608
tryb3l <56679619+tryb3l@users.noreply.github.com>
2024-02-05 23:58:59 +0100

    Refactor smoke test and add buildApp helper function

--- test/smoke-tests/basic-smoke.test.js
+++ test/smoke-tests/basic-smoke.test.js
@@ -4,6 +4,14 @@ const fcli = require("fastify-cli/helper");
 const startArgs = "-l info --options app.js";
 const t = require("tap");
 
+async function buildApp(t, env = envParam, serverOptions) {
+  const app = await fcli.build(startArgs, { configData: env }, serverOptions);
+  t.teardown(() => {
+    app.close();
+  });
+  return app;
+}
+
 t.test("the application should start", async (t) => {
   const envParam = {
     NODE_ENV: "test",
@@ -24,7 +32,7 @@ t.test("the application should start", async (t) => {
 });
 
 t.test("Route is online and working", async (t) => {
-  const app = await fcli.build(t);
+  const app = await buildApp(t);
   const response = await app.inject({
     method: "GET",
     url: "/",
