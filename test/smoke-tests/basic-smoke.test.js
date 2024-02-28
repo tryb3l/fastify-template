e1c32c673fc5b9cbb59a3e0eaad1992d8c8eb0ff
tryb3l <56679619+tryb3l@users.noreply.github.com>
2024-02-29 00:43:39 +0100

    Refactor buildApp function and update MONGO_URL in tests

--- test/smoke-tests/basic-smoke.test.js
+++ test/smoke-tests/basic-smoke.test.js
@@ -5,43 +5,21 @@ const startArgs = "-l info --options app.js";
 const t = require("tap");
 const dockerHelper = require("../helper-docker");
 const docker = dockerHelper();
-
-t.before(async function before() {
-  await docker.startContainer(mongo);
-});
-t.teardown(async () => {
-  await docker.stopContainer(dockerHelper.Containers.mongo);
-});
-
-async function buildApp(t, env = envParam, serverOptions) {
-  const app = await fcli.build(startArgs, { configData: env }, serverOptions);
-  t.teardown(() => {
-    app.close();
-  });
-  return app;
-}
+const { buildApp } = require("../helper");
 
 t.test("the application should start", async (t) => {
-  const envParam = {
-    NODE_ENV: "test",
-    MONGO_URL: "mongodb://localhost:27017/test",
-  };
-
-  const app = await fcli.build(startArgs, {
-    configData: envParam,
-  });
-
-  t.teardown(() => {
-    app.close();
+  const app = await buildApp(t, {
+    MONGO_URL: "mongodb://localhost:27017/basis-test-db",
   });
 
   await app.ready();
-
   t.pass("the application is ready");
 });
 
 t.test("Route is online and working", async (t) => {
-  const app = await buildApp(t);
+  const app = await buildApp(t, {
+    MONGO_URL: "mongodb://localhost:27017/basis-test-db",
+  });
   const response = await app.inject({
     method: "GET",
     url: "/",
