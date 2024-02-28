dca66ea14dcd51c8843e74bc67a5d4712abb6154
tryb3l <56679619+tryb3l@users.noreply.github.com>
2024-02-28 23:19:48 +0100

    Add login and protected route tests

--- test/login.test.js
+++ test/login.test.js
@@ -30,3 +30,29 @@ t.test("register the user", async (t) => {
   t.equal(response.statusCode, 201);
   t.same(response.json(), { registered: true });
 });
+
+t.test("successful login", async (t) => {
+  const app = await buildApp(t);
+  const login = await app.inject({
+    method: "POST",
+    url: "/authenticate",
+    payload: {
+      username: "test",
+      password: "icanpass",
+    },
+  });
+  t.equal(login.statusCode, 200);
+  t.match(login.json(), { token: /(\w*\.){2}.*/ });
+
+  t.test("access protected route", async (t) => {
+    const response = await app.inject({
+      method: "GET",
+      url: "/me",
+      headers: {
+        authorization: `Bearer ${login.json().token}`,
+      },
+    });
+    t.equal(response.statusCode, 200);
+    t.match(response.json(), { username: "John Doe" });
+  });
+});
