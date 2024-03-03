e51012ffb78c2719ad43902802617070eca7ccd8
tryb3l <56679619+tryb3l@users.noreply.github.com>
2024-03-03 15:37:30 +0100

    added req/res hooks in error handler plugin

--- plugins/error-handler.js
+++ plugins/error-handler.js
@@ -2,6 +2,12 @@
 
 const fp = require("fastify-plugin");
 module.exports = fp(function (fastify, opts, next) {
+  fastify.addHook("onRequest", async (req) => {
+    req.log.info({ req }, "incoming request");
+  });
+  fastify.addHook("onResponse", async (req, res) => {
+    req.log.info({ req, res }, "request completed");
+  });
   fastify.setErrorHandler((err, req, reply) => {
     if (reply.statusCode >= 500) {
       req.log.error({ req, res: reply, err: err }, err?.message);
