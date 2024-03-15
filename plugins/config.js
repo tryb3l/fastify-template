003fa1e229dc805e58421128d6c6f395ce732dfc
tryb3l <56679619+tryb3l@users.noreply.github.com>
2024-03-15 23:49:23 +0100

    update app config

--- plugins/config.js
+++ plugins/config.js
@@ -1,25 +1,24 @@
 "use strict";
+
 const fp = require("fastify-plugin");
 const fastifyEnv = require("@fastify/env");
+
 module.exports = fp(
-  async function (fastify, opts, next) {
+  async function registerPluginsAndConfig() {
     await fastify.register(fastifyEnv, {
       confKey: "secrets",
       data: opts.configData,
       schema: fastify.getSchema("schema:dotenv"),
     });
+
     fastify.log.level = fastify.secrets.LOG_LEVEL;
 
     fastify.decorate("config", {
-      mongo: {
-        forceClose: true,
-        url: fastify.secrets.MONGO_URL,
-      },
       jwt: {
         secret: fastify.secrets.JWT_SECRET,
       },
     });
-    next();
   },
+
   { name: "application-config", dependencies: ["application-schemas"] },
 );
