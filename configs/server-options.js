fc889af0bb8f47d51a1a9a78fbe0f39d1f7ece68
tryb3l <56679619+tryb3l@users.noreply.github.com>
2024-03-15 23:50:26 +0100

    update server options

--- configs/server-options.js
+++ configs/server-options.js
@@ -1,17 +1,31 @@
+"use strict";
+const crypto = require("node:crypto");
 const loggerOptions = require("./logger-options");
-module.exports.options = {
+
+module.exports = {
   disableRequestLogging: true,
   logger: loggerOptions,
+  requestIdLogLabel: false,
+  requestIdHeader: "x-request-id",
+  genReqId(req) {
+    return req.headers["x-amz-request-id"] || crypto.randomUUID();
+  },
   ajv: {
     customOptions: {
-      coerceTypes: "array",
       removeAdditional: "all",
-      useDefaults: true,
+      prettyPrint: true,
+      coerceTypes: "array",
+      allErrors: true,
+      nullable: true,
+      unknownFormats: [
+        "int32",
+        "int64",
+        "binary",
+        "byte",
+        "date",
+        "date-time",
+        "password",
+      ],
     },
-  },
-  logger: {
-    prettyPrint: true,
-    level: "debug",
-    disableRequestLogging: false,
   },
 };
