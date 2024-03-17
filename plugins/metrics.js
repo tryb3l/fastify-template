a86edfcfca18e3de36e4bb6e66957cbe922ffc4f
tryb3l <56679619+tryb3l@users.noreply.github.com>
2024-03-17 18:19:53 +0100

    temporary excluding metrics code.

--- plugins/metrics.js
+++ plugins/metrics.js
@@ -1,32 +1,32 @@
-"use strict";
+// "use strict";
 
-const fp = require("fastify-plugin");
-const Fastify = require("fastify");
-module.exports = fp(async function (app) {
-  app.register(require("fastify-metrics"), {
-    defaultMetrics: { enabled: true },
-    endpoint: null,
-    name: "metrics",
-    routeMetrics: { enable: true },
-  });
-  const promServer = Fastify({ logger: app.log });
-  promServer.route({
-    url: "/metrics",
-    method: "GET",
-    logLever: "info",
-    handler: (_, reply) => {
-      reply.type("text/plain");
-      return app.metrics.client.register.metrics();
-    },
-  });
-  app.addHook("onClose", async (instance) => {
-    await promServer.close();
-  });
-  await promServer.listen(
-    {
-      port: 9001,
-      host: "0.0.0.0",
-    },
-    { name: prom },
-  );
-});
+// const fp = require("fastify-plugin");
+// const Fastify = require("fastify");
+// module.exports = fp(async function (app) {
+//   app.register(require("fastify-metrics"), {
+//     defaultMetrics: { enabled: true },
+//     endpoint: null,
+//     name: "metrics",
+//     routeMetrics: { enable: true },
+//   });
+//   const promServer = Fastify({ logger: app.log });
+//   promServer.route({
+//     url: "/metrics",
+//     method: "GET",
+//     logLever: "info",
+//     handler: (_, reply) => {
+//       reply.type("text/plain");
+//       return app.metrics.client.register.metrics();
+//     },
+//   });
+//   app.addHook("onClose", async (instance) => {
+//     await promServer.close();
+//   });
+//   await promServer.listen(
+//     {
+//       port: 9001,
+//       host: "0.0.0.0",
+//     },
+//     //{ name: prom },
+//   );
+// });
