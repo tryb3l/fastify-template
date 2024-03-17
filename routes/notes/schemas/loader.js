3233ad13e8fc015e17bfa413b054311800d9afc4
tryb3l <56679619+tryb3l@users.noreply.github.com>
2024-03-17 18:21:06 +0100

    Add list-export schema to schema loader

--- routes/notes/schemas/loader.js
+++ routes/notes/schemas/loader.js
@@ -3,6 +3,7 @@ const fp = require("fastify-plugin");
 module.exports = fp(async function schemaLoaderPlugin(fastify, opts) {
   fastify.addSchema(require("./list-query.json"));
   fastify.addSchema(require("./list-response.json"));
+  fastify.addSchema(require("./list-export.json"));
   fastify.addSchema(require("./create-body.json"));
   fastify.addSchema(require("./create-response.json"));
   fastify.addSchema(require("./status-params.json"));
