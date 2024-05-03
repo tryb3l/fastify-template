5ea94fd6db3a28af49506720ca6e4d874f2007d2
tryb3l <56679619+tryb3l@users.noreply.github.com>
2024-05-03 17:28:55 +0200

    small tweaks

--- app.js
+++ app.js
@@ -1,22 +1,22 @@
-"use strict";
+'use strict'
 
-const path = require("node:path");
-const AutoLoad = require("@fastify/autoload");
+const path = require('node:path')
+const AutoLoad = require('@fastify/autoload')
 
 // Pass --options via CLI arguments in command to enable these options.
-const options = require("./configs/server-options.js");
+const options = require('./configs/server-options.js')
 
 module.exports = async function (fastify, opts) {
   // Place here your custom code!
-  fastify.log.info("The .env file has been read %s", process.env.MONGO_URL);
+  fastify.log.info('The .env file has been read %s', process.env.MONGO_URL)
 
   fastify.register(AutoLoad, {
-    dir: path.join(__dirname, "schemas"),
+    dir: path.join(__dirname, 'schemas'),
     indexPattern: /^loader.js$/i,
-  });
+  })
 
-  await fastify.register(require("./plugins/config"));
-  fastify.log.info("Config loaded %o", fastify.config);
+  await fastify.register(require('./plugins/config'))
+  fastify.log.info('Config loaded %o', fastify.config)
 
   // Do not touch the following lines
 
@@ -24,23 +24,23 @@ module.exports = async function (fastify, opts) {
   // those should be support plugins that are reused
   // through your application
   fastify.register(AutoLoad, {
-    dir: path.join(__dirname, "plugins"),
+    dir: path.join(__dirname, 'plugins'),
     ignorePattern: /.*.no-load\.js/,
     indexPattern: /^no$/i,
     options: fastify.config,
-  });
+  })
 
   // This loads all plugins defined in routes
   // define your routes in one of these
   fastify.register(AutoLoad, {
-    dir: path.join(__dirname, "routes"),
+    dir: path.join(__dirname, 'routes'),
     indexPattern: /.*routes(\.js|\.cjs)$/i,
     ignorePattern: /.*\.js/,
     autoHooksPattern: /.*hooks(\.js|\.cjs)$/i,
     autoHooks: true,
     cascadeHooks: true,
     options: Object.assign({}, opts),
-  });
-};
+  })
+}
 
-module.exports.options = options;
+module.exports.options = options
