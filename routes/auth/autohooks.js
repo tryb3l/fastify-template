2ef46c5ac659ba049c3ef1f9ab1b0950c94cb9a8
tryb3l <56679619+tryb3l@users.noreply.github.com>
2024-05-10 23:54:28 +0200

    updated style

--- routes/auth/autohooks.js
+++ routes/auth/autohooks.js
@@ -1,27 +1,27 @@
-"use strict";
+'use strict'
 
-const fp = require("fastify-plugin");
-const schemas = require("./schemas/loader");
+const fp = require('fastify-plugin')
+const schemas = require('./schemas/loader')
 
 module.exports = fp(
   async function userAutoHooks(fastify) {
-    const users = fastify.mongo.db.collection("users");
+    const users = fastify.mongo.db.collection('users')
 
-    fastify.register(schemas);
+    fastify.register(schemas)
 
-    fastify.decorate("usersDataSource", {
+    fastify.decorate('usersDataSource', {
       async readUser(username) {
-        const user = await users.findOne({ username });
-        return user;
+        const user = await users.findOne({ username })
+        return user
       },
       async createUser(user) {
-        const newUser = await users.insertOne(user);
-        return newUser.insertedId;
+        const newUser = await users.insertOne(user)
+        return newUser.insertedId
       },
-    });
+    })
   },
   {
     encapsulate: true,
-    dependencies: ["@fastify/mongodb"],
+    dependencies: ['@fastify/mongodb'],
   },
-);
+)
