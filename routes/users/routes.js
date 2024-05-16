3cda837bc89a4bcd4b77a76629e90d23bd574af8
tryb3l <56679619+tryb3l@users.noreply.github.com>
2024-05-16 23:32:23 +0200

    updated user routes

--- routes/users/routes.js
+++ routes/users/routes.js
@@ -6,7 +6,54 @@ module.exports = fp(async function userRoutes(fastify) {
   fastify.addHook('onRequest', fastify.authenticate)
   const users = fastify.mongo.db.collection('users')
 
-  //update user
+  fastify.route({
+    method: 'GET',
+    url: '/',
+    schema: {
+      tags: ['users'],
+      response: {
+        200: {
+          type: 'object',
+          properties: {
+            data: {
+              type: 'array',
+              items: fastify.getSchema('schema:user'),
+            },
+          },
+        },
+        404: { type: 'object', properties: { error: { type: 'string' } } },
+      },
+    },
+    handler: async function listUsers(request, reply) {
+      const users = await this.usersDataSource.listUsers()
+      if (users.length === 0) {
+        reply.code(404)
+        return { error: 'No users found' }
+      }
+      return { data: users }
+    },
+  })
+
+  fastify.route({
+    method: 'GET',
+    url: '/:id',
+    schema: {
+      tags: ['users'],
+      params: fastify.getSchema('schema:user:read:params'),
+      response: {
+        200: fastify.getSchema('schema:user'),
+      },
+    },
+    handler: async function readUser(request, reply) {
+      const user = await this.usersDataSource.readUser(request.params.id)
+      if (!user) {
+        reply.code(404)
+        return { error: 'User not found' }
+      }
+      return user
+    },
+  })
+
   fastify.route({
     method: 'PUT',
     url: '/:id',
@@ -14,18 +61,17 @@ module.exports = fp(async function userRoutes(fastify) {
       tags: ['users'],
       params: fastify.getSchema('schema:user:read:params'),
       body: fastify.getSchema('schema:user:update:body'),
-      handler: async function updateUser(request, reply) {
-        const res = await this.usersDataSource.updateUser(request.params.id.request.body)
-        if (!res) {
-          reply.code(404)
-          return { error: 'User is not found' }
-        }
-        reply.code(200)
-      },
+    },
+    handler: async function updateUser(request, reply) {
+      const res = await this.usersDataSource.updateUser(request.params.id, request.body)
+      if (res.modifiedCount === 0) {
+        reply.code(404)
+        return { error: 'User is not found or not updated' }
+      }
+      reply.code(204)
     },
   })
 
-  //delete user
   fastify.route({
     method: 'DELETE',
     url: '/:id',
@@ -35,9 +81,9 @@ module.exports = fp(async function userRoutes(fastify) {
     },
     handler: async function deleteUser(request, reply) {
       const res = await users.deleteUser({
-        _id: new fastify.mongo.ObjectId(request.params.id),
+        _id: fastify.mongo.ObjectId(request.params.id),
       })
-      if (res.delededCount === 0) {
+      if (res.deletedCount === 0) {
         reply.code(404)
         return { error: 'User is not found' }
       }
