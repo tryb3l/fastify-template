b00adbf44b426ccda82fe1391ee525c96a58eced
tryb3l <56679619+tryb3l@users.noreply.github.com>
2024-05-20 15:40:34 +0200

    updated auth schemas,

--- routes/auth/schemas/loader.js
+++ routes/auth/schemas/loader.js
@@ -1,10 +1,15 @@
-"use strict";
+'use strict'
 
-const fp = require("fastify-plugin");
+const fp = require('fastify-plugin')
 
 module.exports = fp(async function schemaLoaderPlugin(fastify) {
-  fastify.addSchema(require("./register.json"));
-  fastify.addSchema(require("./token-header.json"));
-  fastify.addSchema(require("./token.json"));
-  fastify.addSchema(require("./user.json"));
-});
+  fastify.addSchema(require('./register.json'))
+  fastify.addSchema(require('./token-header.json'))
+  fastify.addSchema(require('./token.json'))
+  fastify.addSchema(require('./user.json'))
+  fastify.addSchema(require('./email.json'))
+  fastify.addSchema(require('./password.json'))
+  fastify.addSchema(require('./username.json'))
+  fastify.addSchema(require('./firstname.json'))
+  fastify.addSchema(require('./lastname.json'))
+})
