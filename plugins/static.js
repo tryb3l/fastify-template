6b69f41033cec14eaa29901817fcc207c423c23e
tryb3l <56679619+tryb3l@users.noreply.github.com>
2024-05-20 15:40:55 +0200

    updated test and static plugin

--- plugins/static.js
+++ plugins/static.js
@@ -7,7 +7,7 @@ const path = require('node:path')
 module.exports = fp(
   async function staticPlugin(fastify) {
     fastify.register(fastifyStatic, {
-      root: path.join(__dirname, '../public'),
+      root: path.join(__dirname, '../frontend/public'),
       prefix: '/public/',
     })
   },
--- test/login.test.js
+++ test/login.test.js
@@ -1,93 +1,94 @@
-"use strict";
+'use strict'
 
-const t = require("tap");
-const { buildApp } = require("./helper");
+const t = require('tap')
+const { buildApp } = require('./helper')
 
-t.test("cannot access protected routes", async (t) => {
+t.test('cannot access protected routes', async (t) => {
   const app = await buildApp(t, {
-    MONGO_URL: "mongodb://localhost:27017/login-test-db",
-  });
-  const privateRoutes = ["/me"];
+    MONGO_URL: 'mongodb://localhost:27017/login-test-db',
+  })
+  const privateRoutes = ['/me']
   for (const url of privateRoutes) {
-    const response = await app.inject({ method: "GET", url });
-    t.equal(response.statusCode, 401);
+    const response = await app.inject({ method: 'GET', url })
+    t.equal(response.statusCode, 401)
     t.same(response.json(), {
       statusCode: 401,
-      error: "Unauthorized",
-      message: "No Authorization was found in request.headers",
-    });
+      error: 'Unauthorized',
+      message: 'No Authorization was found in request.headers',
+    })
   }
-});
+})
 
 function cleanCache() {
   Object.keys(require.cache).forEach(function (key) {
-    delete require.cache[key];
-  });
+    delete require.cache[key]
+  })
 }
-t.test("register error", async (t) => {
-  const path = "../routes/data-store.js";
-  cleanCache();
-  require(path);
+t.test('register error', async (t) => {
+  const path = '../routes/data-store.js'
+  cleanCache()
+  require(path)
   require.cache[require.resolve(path)].exports = {
     async store() {
-      throw new Error("Fail to store");
+      throw new Error('Fail to store')
     },
-  };
+  }
 
-  t.teardown(cleanCache);
+  t.teardown(cleanCache)
   const app = await buildApp(t, {
-    MONGO_URL: "mongodb://localhost:27017/login-test-db",
-  });
+    MONGO_URL: 'mongodb://localhost:27017/login-test-db',
+  })
   const response = await app.inject({
-    url: "/register",
+    url: '/register',
     payload: {
-      username: "test",
-      password: "icanpass",
+      username: 'test',
+      password: 'icanpass',
     },
-  });
-  t.equal(response.statusCode, 500);
-});
+  })
+  t.equal(response.statusCode, 500)
+})
 
-t.test("register the user", async (t) => {
+t.test('register the user', async (t) => {
   const app = await buildApp(t, {
-    MONGO_URL: "mongodb://localhost:27017/login-test-db",
-  });
+    MONGO_URL: 'mongodb://localhost:27017/login-test-db',
+  })
   const response = await app.inject({
-    method: "POST",
-    url: "/register",
+    method: 'POST',
+    url: '/register',
     payload: {
-      username: "test",
-      password: "icanpass",
+      username: 'test',
+      password: 'icanpass',
+      email: 'testbohdantt992t@gmail.com',
     },
-  });
-  t.equal(response.statusCode, 201);
-  t.same(response.json(), { registered: true });
-});
+  })
+  t.equal(response.statusCode, 201)
+  t.same(response.json(), { registered: true })
+})
 
-t.test("successful login", async (t) => {
+t.test('successful login', async (t) => {
   const app = await buildApp(t, {
-    MONGO_URL: "mongodb://localhost:27017/login-test-db",
-  });
+    MONGO_URL: 'mongodb://localhost:27017/login-test-db',
+  })
   const login = await app.inject({
-    method: "POST",
-    url: "/authenticate",
+    method: 'POST',
+    url: '/authenticate',
     payload: {
-      username: "test",
-      password: "icanpass",
+      username: 'test',
+      password: 'icanpass',
     },
-  });
-  t.equal(login.statusCode, 200);
-  t.match(login.json(), { token: /(\w*\.){2}.*/ });
+  })
+  t.equal(login.statusCode, 200)
+  t.match(login.json(), { token: /(\w*\.){2}.*/ })
 
-  t.test("access protected route", async (t) => {
+  t.test('access protected route', async (t) => {
     const response = await app.inject({
-      method: "GET",
-      url: "/me",
+      method: 'GET',
+      url: '/me',
       headers: {
         authorization: `Bearer ${login.json().token}`,
       },
-    });
-    t.equal(response.statusCode, 200);
-    t.match(response.json(), { username: "John Doe" });
-  });
-});
+    })
+    t.equal(response.statusCode, 200)
+    t.match(response.json(), { username: 'John Doe' })
+  })
+})
