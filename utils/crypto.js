e2dfe33d8ff4e804fb00c08c245923faed93cbfb
tryb3l <56679619+tryb3l@users.noreply.github.com>
2024-06-08 13:06:09 +0200

    add generateKey, crcToken, generateToken, validateToken, serializeHash, deserializeHash, hashPassword

--- utils/crypto.js
+++ utils/crypto.js
@@ -43,7 +43,95 @@ const generateUUID = () => {
   return [h1, d2, d3, d4, d5].join('-')
 }
 
+const generateKey = (length, possible) => {
+  const base = possible.length
+  let key = ''
+  for (let i = 0; i < length; i++) {
+    const index = Math.floor(cryptoRandom() * base)
+    key += possible[index]
+  }
+  return key
+}
+
+const CRC_LEN = 4
+
+const crcToken = (secret, key) => {
+  const md5 = crypto.createHash('md5').update(key, secret)
+  return md5.digest('hex').substring(0, CRC_LEN)
+}
+
+const generateToken = (secret, characters, length) => {
+  const key = generateKey(length - CRC_LEN, characters)
+  return key + crcToken(secret, key)
+}
+
+const validateToken = (secret, token) => {
+  if (!token || token.length < CRC_LEN) return false
+  const length = token.length
+  const crc = token.slice(length - CRC_LEN)
+  const key = token.slice(0, -CRC_LEN)
+  return crcToken(secret, key) === crc
+}
+
+const SCRYPT_PARAMS = { N: 32768, r: 8, p: 1, maxmem: 64 * 1024 * 1024, dkLen: 32 }
+const SCRYPT_PREFIX = '$scrypt$N=32768,r=8,p=1$,maxmem=67108864$,dkLen=32$'
+
+const serializeHash = (hash, salt) => {
+  const saltString = salt.toString('base64').split('=')[0]
+  const hashString = hash.toString('base64').split('=')[0]
+  return `${SCRYPT_PREFIX}${saltString}$${hashString}`
+}
+
+const parseOptions = (options) => {
+  const values = []
+  const items = options.split(',')
+  for (const item of items) {
+    const [key, value] = item.split('=')
+    values.push([key, Number(value)])
+  }
+  return Object.fromEntries(values)
+}
+
+const deserializeHash = (phcString) => {
+  const [, name, options, salt64, hash64] = phcString.split('$')
+  if (name !== 'scrypt') {
+    throw new Error('Unsupported hash algorithm, Node.js crypto module only supports scrypt')
+  }
+  const params = parseOptions(options)
+  const salt = Buffer.from(salt64, 'base64')
+  const hash = Buffer.from(hash64, 'base64')
+  return { params, salt, hash }
+}
+
+const SALT_LEN = 32
+const KEY_LEN = 64
+
+const hashPassword = (password) => {
+  new Promise((resolve, reject) => {
+    crypto.randomBytes(SALT_LEN, (err, salt) => {
+      if (err) {
+        reject(err)
+        return
+      }
+      crypto.scrypt(password, salt, KEY_LEN, SCRYPT_PARAMS, (err, hash) => {
+        if (err) {
+          reject(err)
+          return
+        }
+        resolve(serializeHash(hash, salt))
+      })
+    })
+  })
+}
+
 module.exports = {
   cryptoRandom,
   generateUUID,
+  generateKey,
+  crcToken,
+  generateToken,
+  validateToken,
+  serializeHash,
+  deserializeHash,
+  hashPassword,
 }
