0c8a5bdab96d599cfbf6db97adbcfff12b221f75
tryb3l <56679619+tryb3l@users.noreply.github.com>
2024-06-22 16:44:08 +0200

    updated schema loader

--- routes/users/schemas/loader.js
+++ routes/users/schemas/loader.js
@@ -7,6 +7,8 @@ module.exports = fp(async function schemaLoaderPlugin(fastify) {
   fastify.addSchema(require('./update-user.json'))
   fastify.addSchema(require('./users-list-query.json'))
   fastify.addSchema(require('./users-list-respose.json'))
+  fastify.addSchema(require('../../auth/schemas/token-header.json'))
+  fastify.addSchema(require('../../auth/schemas/token.json'))
   fastify.addSchema(require('../../auth/schemas/user.json'))
   fastify.addSchema(require('../../auth/schemas/email.json'))
   fastify.addSchema(require('../../auth/schemas/password.json'))
