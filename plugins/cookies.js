b471359404d20ea9aa3831a7b538e645bf11bac8
tryb3l <56679619+tryb3l@users.noreply.github.com>
2024-07-19 00:26:06 +0200

    updated cookie plugin

--- plugins/cookies.js
+++ plugins/cookies.js
@@ -1,17 +1,20 @@
 'use strict'
 const fp = require('fastify-plugin')
-const cookies = require('@fastify/cookie')
+const cookie = require('@fastify/cookie')
 
-module.exports = fp(async function cookiePlugin(fastify) {
-  fastify.register(cookies, {
-    secret: 'my-secret',
-    parseOptions: {
-      path: '/',
-      maxAge: 60 * 60 * 24,
-      httpOnly: true,
-      sameSite: 'strict',
-      secure: process.env.NODE_ENV === 'production',
-      signed: true,
-    },
-  })
-})
+module.exports = fp(
+  async function cookiePlugin(fastify) {
+    fastify.register(cookie, {
+      secret: fastify.secrets.COOKIE_SECRET || 'my-secret',
+      parseOptions: {
+        path: '/',
+        maxAge: fastify.secrets.COOKIE_MAX_AGE || 1800000,
+        httpOnly: true,
+        sameSite: 'strict',
+        secure: process.env.NODE_ENV === 'production',
+        signed: true,
+      },
+    })
+  },
+  { name: 'cookie-plugin', dependencies: ['application-config'] },
+)
