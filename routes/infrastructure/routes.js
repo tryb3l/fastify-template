0cdb400dade8f984c4e1e7829fd66db8cb6eb2bf
tryb3l <56679619+tryb3l@users.noreply.github.com>
2024-07-22 23:58:33 +0200

    add basic healthcheck endpoint

---
+++ routes/infrastructure/routes.js
@@ -0,0 +1,26 @@
+'use strict'
+
+const fp = require('fastify-plugin')
+
+module.exports = fp(async function infrastructure(fastify) {
+  fastify.route({
+    method: 'GET',
+    url: '/health',
+    schema: {
+      tags: ['infrastructure'],
+      summary: 'Health check',
+      response: {
+        200: {
+          type: 'object',
+          properties: {
+            status: { type: 'string' },
+          },
+        },
+      },
+    },
+    handler: async function healthHandler(request, reply) {
+      reply.code(200)
+      return { status: 'ok' }
+    },
+  })
+})
