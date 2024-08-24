7cf1ca13d35dd4186d8a76a69d0a9d6795d47071
tryb3l <56679619+tryb3l@users.noreply.github.com>
2024-08-24 22:04:27 +0200

    code styling update

--- test/run-after.js
+++ test/run-after.js
@@ -1,11 +1,11 @@
-"use strict";
+'use strict'
 
-const t = require("tap");
-const dockerHelper = require("./helper-docker");
+const t = require('tap')
+const dockerHelper = require('./helper-docker')
 
-const docker = dockerHelper();
-const { Containers } = dockerHelper;
+const docker = dockerHelper()
+const { Containers } = dockerHelper
 
 t.teardown(async () => {
-  await docker.stopContainer(Containers.mongo);
-});
+  await docker.stopContainer(Containers.mongo)
+})
--- test/run-before.js
+++ test/run-before.js
@@ -1,11 +1,11 @@
-"use strict";
+'use strict'
 
-const t = require("tap");
-const dockerHelper = require("./helper-docker");
+const t = require('tap')
+const dockerHelper = require('./helper-docker')
 
-const docker = dockerHelper();
-const { Containers } = dockerHelper;
+const docker = dockerHelper()
+const { Containers } = dockerHelper
 
 t.before(async function before() {
-  await docker.startContainer(Containers.mongo);
-});
+  await docker.startContainer(Containers.mongo)
+})
