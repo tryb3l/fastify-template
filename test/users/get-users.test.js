3936db6820e6e7e39876caa0b4bf073888c75dfa
tryb3l <56679619+tryb3l@users.noreply.github.com>
2024-08-24 22:07:51 +0200

    refactor get users test

--- test/users/get-users.test.js
+++ test/users/get-users.test.js
@@ -1,29 +1,32 @@
 'use strict'
 
 const t = require('tap')
-const { buildApp } = require('../helper')
-const { generateKey } = require('../../utils/crypto')
+const { setup } = require('../utils/setup-user')
 
-const randomUsername = generateKey(8, 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ')
-const randomEmail = `${generateKey(5, 'abcdefghijklmnopqrstuvwxyz')}@${generateKey(5, 'abcdefghijklmnopqrstuvwxyz')}.com`
-const randomPassword = generateKey(
-  12,
-  'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789',
-)
+t.beforeEach(async (t) => {
+  const { app, accessToken, refreshToken } = await setup(t)
+  t.context.app = app
+  t.context.accessToken = accessToken
+  t.context.refreshToken = refreshToken
+})
 
-t.test('register the user', async (t) => {
-  const app = await buildApp(t, {
-    MONGO_URL: 'mongodb://localhost:27017/login-test-db',
-  })
+t.test('GET /users 200', async (t) => {
+  const { app, accessToken, refreshToken } = t.context
+  console.log('accessToken ', accessToken.value)
+  // Act
   const response = await app.inject({
-    method: 'POST',
-    url: '/auth/register',
-    payload: {
-      username: randomUsername,
-      email: randomEmail,
-      password: randomPassword,
+    method: 'GET',
+    url: '/users',
+    headers: {
+      contentType: 'application/json',
+    },
+    cookies: {
+      accessToken: accessToken,
+      refreshToken: refreshToken,
     },
   })
-  t.equal(response.statusCode, 201)
-  t.same(response.json(), { registered: true })
+
+  // Assert
+  t.equal(response.statusCode, 200)
+  t.type(response.json(), 'object')
 })
