58897b918fbd4f162b0d872237d6ecc17fe3db08
tryb3l <56679619+tryb3l@users.noreply.github.com>
2024-08-24 22:08:04 +0200

    code styling update

--- test/helper.js
+++ test/helper.js
@@ -1,35 +1,32 @@
-"use strict";
+'use strict'
 
 // This file contains code that we reuse
 // between our tests.
 
-const fcli = require("fastify-cli/helper");
+const fcli = require('fastify-cli/helper')
 
-const startArgs = "-l silent --options app.js";
+const startArgs = '-l silent --options app.js'
 
 const defaultEnv = {
-  MONGO_URL: "mongodb://localhost:27017/test",
-  NODE_ENV: "test",
-  JWT_SECRET: "secret-11111111",
-};
+  MONGO_URL: 'mongodb://localhost:27017/test',
+  NODE_ENV: 'test',
+  JWT_SECRET: 'secret-11111111',
+}
+
 function config(env) {
-  return { configData: env };
+  return { configData: env }
 }
 
 // automatically build and tear down our instance
 async function buildApp(t, env, serverOptions) {
-  const app = await fcli.build(
-    startArgs,
-    config({ ...defaultEnv, ...env }),
-    serverOptions,
-  );
+  const app = await fcli.build(startArgs, config({ ...defaultEnv, ...env }), serverOptions)
   t.teardown(() => {
-    app.close();
-  });
-  return app;
+    app.close()
+  })
+  return app
 }
 
 module.exports = {
   config,
   buildApp,
-};
+}
