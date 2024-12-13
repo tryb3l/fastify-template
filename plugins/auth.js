c8edef981521af711ba1b8bf946d9298f6264a25
tryb3l <56679619+tryb3l@users.noreply.github.com>
2024-12-13 23:53:38 +0100

    add authentication and authorization features to the auth plugin

--- plugins/auth.js
+++ plugins/auth.js
@@ -2,6 +2,7 @@
 
 const fp = require('fastify-plugin')
 const fastifyJwt = require('@fastify/jwt')
+const { randomUUID } = require('node:crypto')
 
 module.exports = fp(
   async function authenticationPlugin(fastify) {
@@ -26,23 +27,55 @@ module.exports = fp(
     fastify.decorate('authenticate', async function (request, reply) {
       try {
         await request.jwtVerify({ onlyCookie: true })
+
+        const user = await fastify.usersDataSource.readUserById(request.user.sub)
+        if (!user) {
+          throw fastify.httpErrors.unauthorized('User not found')
+        }
+        request.user = user
       } catch (err) {
-        reply.send(err)
+        throw fastify.httpErrors.unauthorized(err.message)
+      }
+    })
+
+    fastify.decorate('authorize', function (allowedRoles) {
+      return async function (request, reply) {
+        if (!allowedRoles.includes(request.user.role)) {
+          request.log.warn(
+            {
+              userId: request.user._id,
+              userRole: request.user.role,
+              requiredRoles: allowedRoles,
+            },
+            'Insufficient permissions',
+          )
+          throw fastify.httpErrors.forbidden('Insufficient permissions')
+        }
+        request.log.info(
+          {
+            userId: request.user._id,
+            userRole: request.user.role,
+          },
+          'Authorization successful',
+        )
       }
     })
 
     fastify.decorateRequest('revokeToken', async function () {
-      revokedTokens.set(this.user.jti, true)
+      const tokenId = this.user.jti
+      revokedTokens.set(tokenId, true)
+      this.log.info({ tokenId }, 'Token revoked')
     })
 
     fastify.decorateRequest('generateAccessToken', async function () {
       const accessToken = fastify.jwt.sign(
         {
-          id: String(this.user._id),
-          username: this.user.username,
+          sub: String(this.user._id),
+          aud: 'access',
+          iss: 'fastify-api',
         },
         {
-          jti: String(Date.now()),
+          jti: randomUUID(),
           expiresIn: fastify.config.jwt.accessExpireIn,
         },
       )
@@ -53,11 +86,12 @@ module.exports = fp(
     fastify.decorateRequest('generateRefreshToken', async function () {
       const refreshToken = fastify.jwt.sign(
         {
-          id: String(this.user._id),
-          username: this.user.username,
+          sub: String(this.user._id),
+          aud: 'refresh',
+          iss: 'fastify-api',
         },
         {
-          jti: String(Date.now()),
+          jti: randomUUID(),
           expiresIn: fastify.config.jwt.refreshExpireIn,
         },
       )
