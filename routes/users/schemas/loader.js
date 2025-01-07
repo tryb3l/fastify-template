f24daf2e01dc714896286079d123e2fb6fa2eb07
tryb3l <56679619+tryb3l@users.noreply.github.com>
2025-01-07 15:52:25 +0100

    update user schemas

--- routes/users/schemas/loader.js
+++ routes/users/schemas/loader.js
@@ -1,18 +1,29 @@
+// 'use strict'
+//
+// const fp = require('fastify-plugin')
+//
+// module.exports = fp(async function schemaLoaderPlugin(fastify) {
+//   fastify.addSchema(require('./read-params.json'))
+//   fastify.addSchema(require('./update-user.json'))
+//   fastify.addSchema(require('./users-list-query.json'))
+//   fastify.addSchema(require('./users-list-response.json'))
+// }, { name: 'users-schema-loader' })
 'use strict'
 
-const fp = require('fastify-plugin')
+function loadUserSchemas(fastify) {
+  console.log('Loading users schemas');
+  const schemas = [
+    './read-params.json',
+    './update-user.json',
+    './users-list-query.json',
+    './users-list-response.json',
+  ];
+  for (const schemaPath of schemas) {
+    const schema = require(schemaPath);
+    fastify.addSchema(schema);
+    console.log(`User schema added: ${schema.$id}`);
+  }
+  console.log('Users schemas loaded');
+}
 
-module.exports = fp(async function schemaLoaderPlugin(fastify) {
-  fastify.addSchema(require('./read-params.json'))
-  fastify.addSchema(require('./update-user.json'))
-  fastify.addSchema(require('./users-list-query.json'))
-  fastify.addSchema(require('./users-list-respose.json'))
-  fastify.addSchema(require('../../auth/schemas/token-header.json'))
-  fastify.addSchema(require('../../auth/schemas/token.json'))
-  fastify.addSchema(require('../../auth/schemas/user.json'))
-  fastify.addSchema(require('../../auth/schemas/email.json'))
-  fastify.addSchema(require('../../auth/schemas/password.json'))
-  fastify.addSchema(require('../../auth/schemas/username.json'))
-  fastify.addSchema(require('../../auth/schemas/firstname.json'))
-  fastify.addSchema(require('../../auth/schemas/lastname.json'))
-})
+module.exports = { loadUserSchemas }
