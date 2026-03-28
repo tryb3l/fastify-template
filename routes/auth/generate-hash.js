40c2d529c1fb25d62578c19015c32576fd2a442a
Bohdan Trybel <bogdan.trybel@gmail.com>
2026-03-28 10:03:19 +0100

    refactor `generate-hash` to improve code structure and remove unnecessary comments

--- routes/auth/generate-hash.js
+++ routes/auth/generate-hash.js
@@ -5,13 +5,26 @@ const util = require('node:util')
 
 const pbkdf2 = util.promisify(crypto.pbkdf2)
 
-module.exports = async function generateHash(password, salt) {
+async function generateHash(password, salt) {
   if (!salt) {
-    // Generate a random salt value
     salt = crypto.randomBytes(16).toString('hex')
   }
-
-  // Hash the password using the salt value and SHA-256 algorithm
   const hash = (await pbkdf2(password, salt, 1000, 64, 'sha256')).toString('hex')
   return { salt, hash }
+}
+
+async function verifyPassword(submittedPassword, storedSalt, storedHash) {
+  const { hash: computedHash } = await generateHash(submittedPassword, storedSalt)
+
+  const computedBuffer = Buffer.from(computedHash, 'hex')
+  const storedBuffer = Buffer.from(storedHash, 'hex')
+
+  if (computedBuffer.length !== storedBuffer.length) return false
+
+  return crypto.timingSafeEqual(computedBuffer, storedBuffer)
+}
+
+module.exports = {
+  generateHash,
+  verifyPassword
 }
