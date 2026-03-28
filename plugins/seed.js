cf7f53a9bd3c4eeeb3f3b865c6fe7825b7b6f18f
Bohdan Trybel <bogdan.trybel@gmail.com>
2026-03-28 10:01:50 +0100

    add `seed` plugin for initial database seeding and refactor `user authentication` to support email or username

---
+++ plugins/seed.js
@@ -0,0 +1,34 @@
+'use strict'
+
+const fp = require('fastify-plugin')
+const { generateHash } = require('../routes/auth/generate-hash')
+
+module.exports = fp(async function seedPlugin(fastify) {
+    fastify.log.info("Checking database for initial seed data...")
+
+    const usersCollection = fastify.mongo.db.collection('users')
+    const adminExists = await usersCollection.findOne({ email: fastify.config.ADMIN_EMAIL })
+
+    if (!adminExists) {
+        fastify.log.info("No admin user found. Seeding default admin account...")
+        const { salt, hash } = await generateHash(fastify.config.ADMIN_PASSWORD)
+
+        await usersCollection.insertOne({
+            email: fastify.config.ADMIN_EMAIL,
+            password: hash,
+            salt: salt,
+            role: 'admin',
+            createdAt: new Date(),
+            updatedAt: new Date()
+        })
+
+        fastify.log.info("Default admin account seeded successfully!")
+    } else {
+        fastify.log.info("Admin account already exists. Skipping seed.")
+    }
+
+}, {
+    name: 'seed-plugin',
+    dependencies: ['db-plugin', 'application-config'],
+    decorators: { fastify: ['mongo', 'config'] }
+})
