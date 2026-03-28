eab73860355cb99bea548dbee253d0da530dea14
Bohdan Trybel <bogdan.trybel@gmail.com>
2026-03-28 10:00:11 +0100

    update `CORS` plugin to use allowed origins from config

--- plugins/cors.js
+++ plugins/cors.js
@@ -1,23 +1,24 @@
 'use strict'
-/**
- * Fastify plugin for enabling Cross-Origin Resource Sharing (CORS).
- *
- * @module corsPlugin
- * @param {Object} fastify - The Fastify instance.
- * @param {Object} opts - The options object.
- * @returns {Promise<void>} - A promise that resolves when the plugin is registered.
- */
 
 const fp = require('fastify-plugin')
 const fastifyCors = require('@fastify/cors')
 
 module.exports = fp(async function corsPlugin(fastify) {
-  fastify.register(fastifyCors, {
-    origin: true,
+  fastify.log.info('Starting registration of cors plugin')
+
+  const allowedOrigins = fastify.config.FRONTEND_URL.split(',').map(s => s.trim())
+
+  await fastify.register(fastifyCors, {
+    origin: allowedOrigins,
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     allowedHeaders: ['Content-Type', 'Authorization'],
     exposedHeaders: ['Set-Cookie'],
   })
-},
-  { name: 'cors-plugin' })
+
+  fastify.log.info('Successfully registered cors plugin')
+}, {
+  name: 'cors-plugin',
+  dependencies: ['application-config'],
+  decorators: { fastify: ['config'] }
+})
