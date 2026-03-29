d4cc2fb47b09e72b1e934447cb225aacdfbe2fe1
Bohdan Trybel <bogdan.trybel@gmail.com>
2026-03-29 19:35:02 +0200

    add `CSRF` protection plugin with conf

---
+++ plugins/csrf.js
@@ -0,0 +1,24 @@
+'use strict'
+
+const fp = require('fastify-plugin')
+const fastifyCsrfProtection = require('@fastify/csrf-protection')
+
+module.exports = fp(async function csrfPlugin(fastify) {
+  fastify.log.info('Starting registration of csrf plugin')
+
+  await fastify.register(fastifyCsrfProtection, {
+    cookieOpts: {
+      signed: false,
+      httpOnly: true,
+      sameSite: 'lax',
+      secure: fastify.config.NODE_ENV === 'production'
+    },
+    sessionPlugin: '@fastify/cookie'
+  })
+
+  fastify.log.info('Successfully registered csrf plugin')
+}, {
+  name: 'csrf-plugin',
+  dependencies: ['application-config', 'cookie-plugin'],
+  decorators: { fastify: ['config'] }
+})
