0cf62e9b344337f93706b6de9fe07bc788dd61f5
Bohdan Trybel <bogdan.trybel@gmail.com>
2026-04-06 18:02:45 +0200

    Refactor test environment configuration to utilize environment variables for mongo url  and JWT secrets. update registration tests to use dynamic mongo urls

--- test/helper.js
+++ test/helper.js
@@ -4,14 +4,18 @@ const fcli = require('fastify-cli/helper')
 
 const startArgs = '-l silent --options app.js'
 
-const defaultEnv = {
-  MONGO_URL: 'mongodb://localhost:27017/test',
-  NODE_ENV: 'test',
-  JWT_SECRET: 'secret-11111111',
+function getDefaultEnv() {
+  return {
+    MONGO_URL: process.env.MONGO_URL || 'mongodb://localhost:27017/test',
+    NODE_ENV: 'test',
+    JWT_SECRET: process.env.JWT_SECRET || 'secret-11111111',
+    COOKIE_SECRET: process.env.COOKIE_SECRET || 'cookie-secret-11111111',
+    FRONTEND_URL: process.env.FRONTEND_URL || 'http://localhost:5173',
+  }
 }
 
 function getTestEnv(env = {}) {
-  return { ...defaultEnv, ...env }
+  return { ...getDefaultEnv(), ...env }
 }
 
 function getTestMongoUrl(env = {}) {
@@ -25,11 +29,11 @@ function config(env) {
 // automatically build and tear down our instance
 async function buildApp(t, env, serverOptions) {
   const app = await fcli.build(startArgs, config(getTestEnv(env)), serverOptions)
-  
+
   t.after(async () => {
     await app.close()
   })
-  
+
   return app
 }
 
